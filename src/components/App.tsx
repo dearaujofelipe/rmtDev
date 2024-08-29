731520b80939de14ab3897bd2bed9b8e13e0bf36
dearaujofelipe <mgdefelipe@outlook.com>
2024-08-29 18:07:26 -0300

    children pattern vs prop drilling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,74 @@
+import { useEffect, useState } from 'react';
 import Background from './Background';
 import Container from './Container';
 import Footer from './Footer';
-import Header from './Header';
+import Header, { HeaderTop } from './Header';
+import Logo from './Logo';
+import BookmarksButton from './BookmarksButton';
+import SearchForm from './SearchForm';
+import Sidebar, { SidebarTop } from './Sidebar';
+import JobItemContent from './JobItemContent';
+import JobList from './JobList';
+import PaginationControls from './PaginationControls';
+import ResultsCount from './ResultsCount';
+import SortingControls from './SortingControls';
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  const [jobItems, setJobItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async (searchText: string) => {
+      if (!searchText) return;
+
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+        );
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await res.json();
+        setJobItems(data.jobItems);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData(searchText);
+
+    return () => {
+      setIsLoading(false);
+    };
+  }, [searchText]);
+
   return (
     <>
       <Background />
-      <Header />
-      <Container />
+
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+          <JobList jobItems={jobItems} isLoading={isLoading} />
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
+
       <Footer />
     </>
   );
