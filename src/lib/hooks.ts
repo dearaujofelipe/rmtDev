dd448f77410883c4b8e621b90105b1f012c57d29
dearaujofelipe <mgdefelipe@outlook.com>
2024-09-01 19:57:11 -0300

    fix types issues

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
+import { JobItem } from './types';
 
 export function useJobItems(searchText: string) {
-  const [jobItems, setJobItems] = useState([]);
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const jobItemsSliced = jobItems.slice(0, 7);
 
@@ -32,8 +33,5 @@ export function useJobItems(searchText: string) {
     };
   }, [searchText]);
 
-  return {
-    jobItemsSliced,
-    isLoading,
-  };
+  return [jobItemsSliced, isLoading] as const;
 }
